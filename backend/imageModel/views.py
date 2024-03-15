b020509a683f4837e4abd3d506d825dd3fdbcac1
th3bossc <diljith2003@gmail.com>
2024-03-16 01:03:57 +0530

    made sure temporary files are deleted

--- backend/imageModel/views.py
+++ backend/imageModel/views.py
@@ -1,17 +1,17 @@
 from rest_framework.views import APIView 
-from rest_framework.response import Response
-from .utils.main import colorize, save_image, delete_image
+from .utils.main import colorize, save_image, cleanup
 from django.http import FileResponse
 # Create your views here
 
 class ImageModelView(APIView):
-    def get(self, request):
+    def get(self):
+        cleanup()
         output = colorize("sample.jpg")
-        # return Response.json({"message": "Hello, world!"})
         return FileResponse(open(output, "rb"), content_type="image/jpeg")
     
     
     def post(self, request):
+        cleanup()
         image_data = request.FILES.get('image')   
         image_path = save_image(image_data, "input.jpg")     
         
