48c409371e7fc12acaa993381fce9c81a8008194
th3bossc <diljith2003@gmail.com>
2024-03-16 16:51:31 +0530

    made minor modification

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,10 @@
-import UpperHero from "@/components/UpperHero";
-import LowerHero from "@/components/LowerHero";
 import Drag from "@/components/Drag";
+import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <div className="App">
-      <UpperHero />
-      <div className="lower">
-        <LowerHero />
-        <div>
-          <Drag />
-        </div>
-      </div>
+      <HeroSection />
+      <Drag />
     </div>
   );
 }
--- frontend/src/components/LowerHero/index.tsx
+++ frontend/src/components/LowerHero/index.tsx
@@ -2,46 +2,24 @@
 
 import styles from "./LowerHero.module.css";
 import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-import useScrollPosition from "@/hooks/useScroll";
 import Image from "next/image";
 
-export default function LowerHero() {
-    const [scrollYProgress, setScrollYProgress] = useState(0);
-    const ref = useRef(null);
-    const { scrollPosition, height, windowHeight } = useScrollPosition({ ref });
-    console.log(scrollPosition, height)
-
-
-    useEffect(() => {
-        const calculateScrollYProgress = () => {
-            console.log(scrollPosition);
-            if (scrollPosition > 0)
-                return 0;
-            if (scrollPosition < -height + windowHeight)
-                return 100;
-            return Math.abs(scrollPosition / (height - windowHeight)) * 100;
-        }
-
-        setScrollYProgress(calculateScrollYProgress());
-
-    }, [scrollPosition, height, windowHeight]);
-
-
-
+export default function LowerHero({
+    scrollYProgress
+}: {
+    scrollYProgress: number
+}) {
 
     return (
-        <div className={styles.lowerHero} ref={ref}>
-            <div className={styles['container']}>
-                <div className={styles["sticky-div"]}>
-                    <div className={styles["grayDiv"]}>
-                        <Image className={`${styles['art']} ${styles['gray']}`} src="/HeroArtGray.png" alt="Hero Art Gray" width={1000} height={1000} />
-                    </div>
-                    <div className={styles["colorDiv"]}>
-                        <motion.img
-                            style={{ clipPath: `polygon(0 0, ${scrollYProgress}% 0,${scrollYProgress}% 100%, 0 100%)` }}
-                            className={`${styles['art']} ${styles['colored']}`} src="/HeroArt.jpg" alt="Hero Art" />
-                    </div>
+        <div className={styles.lowerHero}>
+            <div className={styles["sticky-div"]}>
+                <div className={styles["grayDiv"]}>
+                    <Image className={`${styles['art']} ${styles['gray']}`} src="/HeroArtGray.png" alt="Hero Art Gray" width={1000} height={1000} />
+                </div>
+                <div className={styles["colorDiv"]}>
+                    <motion.img
+                        style={{ clipPath: `polygon(0 0, ${scrollYProgress}% 0,${scrollYProgress}% 100%, 0 100%)` }}
+                        className={`${styles['art']} ${styles['colored']}`} src="/HeroArt.jpg" alt="Hero Art" />
                 </div>
             </div>
         </div>
