82cdbd2a9da2bb9c9f483b469840b6477e459f9e
Madhav-Murali-NITC <madhav_b210027cs@nitc.ac.in>
2024-03-16 14:12:53 +0530

    Added scribble with animations

---
+++ frontend/src/app/components/Drag.tsx
@@ -0,0 +1,22 @@
+
+'use client';
+import leftSVG from "/public/leftSVG.svg";
+import Image from "next/image";
+import scribble from "/public/scribble.svg";
+import Spline from '@splinetool/react-spline';
+import Scribble from "./Scribble";
+
+
+export default function Drag()
+{
+    return (
+        <div className="drag--container">
+            <Image className="drag--svg" src={leftSVG} alt="rightSVG" />
+            <div className="drag--scribble--div">
+            <h2 className="drag--text">Drag and drop your image here</h2>
+            <Scribble />
+            </div>
+            <Image className="drag--svg" src={leftSVG} alt="rightSVG" />
+        </div>
+    );
+}
