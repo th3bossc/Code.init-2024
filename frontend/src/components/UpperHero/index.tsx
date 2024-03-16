e7b7332252d779bb7f41b8524114693537f9daf6
Ashwin S <ashwinsbabu@hotmail.com>
2024-03-17 00:56:54 +0530

    Sultan done I think

--- frontend/src/components/UpperHero/index.tsx
+++ frontend/src/components/UpperHero/index.tsx
@@ -2,12 +2,59 @@ import "./UpperHero.css"
 import leftSVG from "/public/leftSVG.svg";
 import rightSVG from "/public/rightSVG.svg";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function UpperHero()
 {
+
+    
     return (
         <div className="upperHero">
-            <h1 className="vitalyze">VITAL<span className="Y">Y</span>ZE</h1>
+            <motion.h1 className="vitalyze">
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.1}}
+                >V</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.15}}>I</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.2}}>T</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.25}}>A</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.3}}>L</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.35}} className="Y">
+                    Y
+                </motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.4}}>Z</motion.span>
+                <motion.span
+                    style={{display:"inline-block"}}
+                    initial={{y:-80,opacity:0}}
+                    animate={{y:0,opacity:1}}
+                    transition={{duration:0.3,delay:0.45}}>E</motion.span>
+            </motion.h1>
             <Image className="svgLeft" src={leftSVG} alt="leftSVG" />
             <Image className="svgRight" src={rightSVG} alt="rightSVG" />
             <h2 className="vitalyzeSubText">convert black and white images into <span className="colourized">colourized</span> versions with realistic and aesthetically pleasing results</h2>
