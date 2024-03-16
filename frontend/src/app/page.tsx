9dd6bf8797fb0c38cdbcbd988e9e7fbaee4e338c
Ashwin S <ashwinsbabu@hotmail.com>
2024-03-16 17:50:14 +0530

    Fixed goofy animation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Drag from "../components/Drag";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Generated from "@/components/Generated";
+
 export default function Home() {
   return (
     <div className="App">
       <HeroSection />
       <Drag />
+      <Generated />
       <Footer />
     </div>
   );
--- frontend/src/components/Generated/index.tsx
+++ frontend/src/components/Generated/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import React from 'react';
 import "./Generated.css";
+import sample from "/public/HeroArt.jpg";
+import ReactCompareImage from 'react-compare-image';
 
 export default function Generated(){
     return (
         <div className="generatedContainer">
-            <div className="generated"></div>
+            <div className="generated">
+                <div className="generatedImage">
+                    <div className="w-full"> 
+                        <ReactCompareImage leftImage="/HeroArtGray.png" rightImage="/HeroArt.jpg" handleSize={18} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
